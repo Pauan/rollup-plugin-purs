411f1bfad1644ad5a31ef6211cc98b006736f688
Pauan <pcxunlimited@gmail.com>
2016-12-26 00:34:52 -1000

    Replacing foo["bar"] with foo.bar

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ function toIdentifier(x) {
 
 function exportVar(imports, identifier, expression) {
   if (expression.type === "Identifier") {
+    // export { expression as identifier };
     // TODO source maps
     return b.exportNamedDeclaration(null, [
       b.exportSpecifier(expression, identifier)
@@ -39,6 +40,7 @@ function exportVar(imports, identifier, expression) {
       var from = toIdentifier(expression.property);
 
       if (from !== null) {
+        // export { from as identifier } from file;
         // TODO source maps
         return b.exportNamedDeclaration(null, [
           b.exportSpecifier(from, identifier)
@@ -47,6 +49,7 @@ function exportVar(imports, identifier, expression) {
     }
   }
 
+  // export var identifier = expression;
   // TODO source maps
   return b.exportNamedDeclaration(b.variableDeclaration("var", [
     b.variableDeclarator(identifier, expression)
@@ -71,96 +74,120 @@ module.exports = function (options) {
 
       var imports = {};
 
-      var node = ast.program;
+      $recast.types.visit(ast, {
+        visitProgram: function (path) {
+          var node = path.node;
 
-      var body = [];
+          var body = [];
 
-      node.body.forEach(function (x) {
-        if (x.type === "VariableDeclaration") {
-          var declarations = [];
+          node.body.forEach(function (x) {
+            if (x.type === "VariableDeclaration") {
+              var declarations = [];
 
-          x.declarations.forEach(function (x) {
-            // TODO handle other patterns rather than only identifiers ?
-            // var foo = require("bar");
-            if (x.id.type === "Identifier" &&
-                x.init !== null &&
-                x.init.type === "CallExpression" &&
-                x.init.callee.type === "Identifier" &&
-                x.init.callee.name === "require" &&
-                x.init.arguments.length === 1 &&
-                x.init.arguments[0].type === "Literal" &&
-                typeof x.init.arguments[0].value === "string") {
-              var file = x.init.arguments[0];
+              x.declarations.forEach(function (x) {
+                // TODO handle other patterns rather than only identifiers ?
+                // var foo = require("bar");
+                if (x.id.type === "Identifier" &&
+                    x.init !== null &&
+                    x.init.type === "CallExpression" &&
+                    x.init.callee.type === "Identifier" &&
+                    x.init.callee.name === "require" &&
+                    x.init.arguments.length === 1 &&
+                    x.init.arguments[0].type === "Literal" &&
+                    typeof x.init.arguments[0].value === "string") {
+                  var file = x.init.arguments[0];
 
-              imports[x.id.name] = file;
+                  imports[x.id.name] = file;
 
-              // TODO source maps
-              body.push(b.importDeclaration([
-                b.importNamespaceSpecifier(x.id)
-              ], file));
+                  // TODO source maps
+                  body.push(b.importDeclaration([
+                    b.importNamespaceSpecifier(x.id)
+                  ], file));
 
-            } else {
-              declarations.push(x);
-            }
-          });
+                } else {
+                  declarations.push(x);
+                }
+              });
 
-          if (declarations.length !== 0) {
-            x.declarations = declarations;
-            body.push(x);
-          }
+              if (declarations.length !== 0) {
+                x.declarations = declarations;
+                body.push(x);
+              }
 
-        // "use strict";
-        } else if (x.type === "ExpressionStatement" &&
-                   x.expression.type === "Literal" &&
-                   x.expression.value === "use strict") {
-          // Do nothing
+            // "use strict";
+            } else if (x.type === "ExpressionStatement" &&
+                       x.expression.type === "Literal" &&
+                       x.expression.value === "use strict") {
+              // Do nothing
 
-        } else if (x.type === "ExpressionStatement" &&
-                   x.expression.type === "AssignmentExpression" &&
-                   x.expression.operator === "=" &&
-                   x.expression.left.type === "MemberExpression" &&
-                   x.expression.left.object.type === "Identifier") {
-          if (x.expression.left.object.name === "exports") {
-            // TODO what about computed expressions ?
-            var identifier = toIdentifier(x.expression.left.property);
+            } else if (x.type === "ExpressionStatement" &&
+                       x.expression.type === "AssignmentExpression" &&
+                       x.expression.operator === "=" &&
+                       x.expression.left.type === "MemberExpression" &&
+                       x.expression.left.object.type === "Identifier") {
+              if (x.expression.left.object.name === "exports") {
+                // TODO what about computed expressions ?
+                var identifier = toIdentifier(x.expression.left.property);
 
-            // exports.foo = bar;
-            if (identifier !== null) {
-              body.push(exportVar(imports, identifier, x.expression.right));
+                // exports.foo = bar;
+                if (identifier !== null) {
+                  body.push(exportVar(imports, identifier, x.expression.right));
+
+                } else {
+                  body.push(x);
+                }
+
+              // module.exports = foo;
+              } else if (x.expression.left.object.name === "module") {
+                // module.exports = { ... };
+                if (x.expression.right.type === "ObjectExpression") {
+                  x.expression.right.properties.forEach(function (x) {
+                    // TODO what about computed expressions ?
+                    var identifier = toIdentifier(x.key);
+
+                    // foo: bar
+                    if (identifier !== null) {
+                      // TODO handle get/set different ?
+                      body.push(exportVar(imports, identifier, x.value));
+                    }
+                  });
+                }
+
+                // export default foo;
+                // TODO source maps
+                body.push(b.exportDefaultDeclaration(x.expression.right));
+
+              } else {
+                body.push(x);
+              }
 
             } else {
               body.push(x);
             }
+          });
 
-          // module.exports = foo;
-          } else if (x.expression.left.object.name === "module") {
-            // module.exports = { ... };
-            if (x.expression.right.type === "ObjectExpression") {
-              x.expression.right.properties.forEach(function (x) {
-                // TODO what about computed expressions ?
-                var identifier = toIdentifier(x.key);
+          node.body = body;
 
-                // foo: bar
-                if (identifier !== null) {
-                  // TODO handle get/set different ?
-                  body.push(exportVar(imports, identifier, x.value));
-                }
-              });
-            }
+          this.traverse(path);
+        },
 
+        visitMemberExpression: function (path) {
+          var node = path.node;
+
+          // foo["bar"] = qux;
+          if (node.computed &&
+              node.property.type === "Literal" &&
+              typeof node.property.value === "string" &&
+              isValidIdentifier(node.property.value)) {
+            node.computed = false;
+            // foo.bar = qux;
             // TODO source maps
-            body.push(b.exportDefaultDeclaration(x.expression.right));
-
-          } else {
-            body.push(x);
+            node.property = b.identifier(node.property.value);
           }
 
-        } else {
-          body.push(x);
+          this.traverse(path);
         }
       });
-
-      node.body = body;
 
       var out = $recast.print(ast);
 
