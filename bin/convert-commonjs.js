f1559e6e5af2b6baf47ff1b47d6dd01c20752edb
Pauan <pcxunlimited@gmail.com>
2017-01-10 01:12:22 -1000

    Adding in convert-commonjs binary

---
+++ bin/convert-commonjs.js
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+var $fs = require("fs");
+var $convert = require("../src/convert");
+
+
+var args = process.argv.slice(2);
+
+var filename = args[0];
+
+var context = {
+  warn: function (code) {
+    console.warn("Warning: " + code);
+  }
+};
+
+$fs.readFile(filename, { encoding: "utf8" }, function (err, file) {
+  if (err) {
+    throw err;
+
+  } else {
+    console.log($convert.call(context, file, filename).code);
+  }
+});
